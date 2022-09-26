b1480e99283c66c038fa39f97b324088d770cfbd
yahuarcani <hjsarmiento1999@gmail.com>
2022-09-26 13:38:21 -0500

    initial commit

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse('Hello World')
+	return HttpResponse('Estás en la página principal de Premios Platzi App')
 
 
+def detail(request, question_id):
+	return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+	return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+	return HttpResponse(f"Estás votando a la pregunta número {question_id}")
